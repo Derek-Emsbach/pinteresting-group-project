2fcf1e7af672939e27c19cf9aa0ff077313f1960
ChrisC <chrischan8551@gmail.com>
2023-01-30 22:06:00 -0600

    fixed bugs in front end

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,107 +10,76 @@ import { authenticate } from './store/session';
 import PinterestLayout from './components/Pinterest_layout/PinterestLayout';
 import HomePage from './components/Pages/HomePage/HomePage';
 import Profile from './components/Pages/ProfilePage/Profile';
-import CreatePinForm from './components/Forms/CreatePinForm'
+import CreatePinForm from './components/Forms/CreatePinForm';
 import PinPage from './components/Pages/PinPage';
 import BoardDetailPage from './components/Pages/BoardDetailPage';
 import CreateBoardForm from './components/Forms/CreateBoardForm';
 import EditProfileForm from './components/Forms/EditProfileForm';
 import { useParams } from 'react-router-dom';
-import User from './components/User/User'
-
+import User from './components/User/User';
 
 function App() {
-  const [isLoaded, setisLoaded] = useState(false);
-  const dispatch = useDispatch();
-  const sessionUser = useSelector(state =>state.session.user)
+	const [isLoaded, setisLoaded] = useState(false);
+	const dispatch = useDispatch();
+	const sessionUser = useSelector((state) => state.session.user);
 
+	useEffect(() => {
+		(async () => {
+			await dispatch(authenticate());
+			setisLoaded(true);
+		})();
+	}, [dispatch]);
 
-  useEffect(() => {
-    (async () => {
-      await dispatch(authenticate());
-      setisLoaded(true);
-    })();
-  }, [dispatch]);
+	if (!isLoaded) {
+		return null;
+	}
 
-  if (!isLoaded) {
-    return null;
-  }
+	return (
+		<div>
+			<Route exact path='/'>
+				<HomePage />
+			</Route>
 
-  return (
-    <div>
-
-
-      <Route exact path="/">
-        <HomePage />
-      </Route>
-
-
-    {sessionUser&&(
-      <Navigation isLoaded={isLoaded} />
-      )}
-      {isLoaded &&(
-        <Switch>
-        {sessionUser &&(
-        <Route exact path= {`/${sessionUser.username}`}>
-           <Profile/>
-         </Route>
-         )}
-
-          <Route exact path="/">
-            <PinterestLayout />
-          </Route>
-
-          <Route exact path="/login">
-            <LoginForm />
-          </Route>
-          <Route exact path="/signup">
-            <SignUpForm />
-          </Route>
-            {/* <Route path='/pins'>
+			{sessionUser && <Navigation isLoaded={isLoaded} />}
+			{isLoaded && (
+				<Switch>
+					{sessionUser && (
+						<Route exact path={`/${sessionUser.username}`}>
+							<Profile />
+						</Route>
+					)}
+					<Route exact path='/'>
+						<PinterestLayout />
+					</Route>
+					<Route exact path='/login'>
+						<LoginForm />
+					</Route>
+					<Route exact path='/signup'>
+						<SignUpForm />
+					</Route>
+					{/* <Route path='/pins'>
               <PinDetailPage />
             </Route> */}
-            <ProtectedRoute exact path='/pins/:pinId'>
-              <PinDetailPage />
-            </ProtectedRoute>
-
-            <ProtectedRoute exact path='/boards'>
-              <BoardDetailPage/>
-            </ProtectedRoute>
-
-            <ProtectedRoute exact path='/boards/:boardId'>
-              <BoardDetailPage/>
-            </ProtectedRoute>
-
-          <Route exact path="/pins">
-            <PinPage />
-          </Route>
-
-          <Route exact path="/pinform">
-            <CreatePinForm />
-          </Route>
-
-          <Route exact path="/boards">
-          <BoardDetailPage />
-        </Route>
-
-        <Route exact path="/boardform">
-        <CreateBoardForm />
-      </Route>
-
-      <Route exact path="/profileform">
-      <EditProfileForm />
-    </Route>
-
-    <Route exact path="/users">
-    <UsersList />
-  </Route>
-
-<Route exact path="/users/:userId">
-<User />
-</Route>
-
-
-          {/*
+					={' '}
+					<ProtectedRoute exact path='/boards'>
+						<BoardDetailPage />
+					</ProtectedRoute>
+					<ProtectedRoute exact path='/boards/:boardId'>
+						<BoardDetailPage />
+					</ProtectedRoute>
+					<Route exact path='/pins'>
+						<PinPage />
+					</Route>
+					<Route exact path='/pinform'>
+						<CreatePinForm />
+					</Route>
+					<Route exact path='/boardform'>
+						<CreateBoardForm />
+					</Route>
+					<Route exact path='/profileform'>
+						<EditProfileForm />
+					</Route>
+					{/*
             <Route path='/following`'>
               <FollowingPage/>
             </Route>
@@ -118,20 +87,16 @@ function App() {
               <FollowersPage/>
             </Route>
           */}
-					 <ProtectedRoute exact path='/users'>
-							<UsersList />
-						</ProtectedRoute>
-						<ProtectedRoute exact path='/users/:userId'>
-							<User />
-						</ProtectedRoute>
-
-        </Switch>
-        )}
-
-
-
-    </div>
-  );
+					<ProtectedRoute exact path='/users'>
+						<UsersList />
+					</ProtectedRoute>
+					<ProtectedRoute exact path='/users/:userId'>
+						<User />
+					</ProtectedRoute>
+				</Switch>
+			)}
+		</div>
+	);
 }
 
 export default App;
