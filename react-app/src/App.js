bef81667c8cde98256f9924e2d46a854f455d843
Aman Kaur <amanpreet_k32@yahoo.com>
2023-02-01 21:19:38 -0800

    board detail page partially completed

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,85 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import LoginForm from './components/auth/LoginForm';
-import SignUpForm from './components/auth/SignUpForm';
-import Navigation from './components/Navigation';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import UsersList from './components/User/UsersList';
-import { authenticate } from './store/session';
-import PinterestLayout from './components/Pinterest_layout/PinterestLayout';
-import HomePage from './components/Pages/HomePage/HomePage';
-import Profile from './components/Pages/ProfilePage/Profile';
-import CreatePinForm from './components/Forms/CreatePinForm';
-import PinPage from './components/Pages/PinPage';
-import BoardDetailPage from './components/Pages/BoardDetailPage';
-import CreateBoardForm from './components/Forms/CreateBoardForm';
-import EditProfileForm from './components/Forms/EditProfileForm';
-import { useParams } from 'react-router-dom';
-import User from './components/User/User';
+import React, { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import LoginForm from "./components/auth/LoginForm";
+import SignUpForm from "./components/auth/SignUpForm";
+import Navigation from "./components/Navigation";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import UsersList from "./components/User/UsersList";
+import { authenticate } from "./store/session";
+import PinterestLayout from "./components/Pinterest_layout/PinterestLayout";
+import HomePage from "./components/Pages/HomePage/HomePage";
+import Profile from "./components/Pages/ProfilePage/Profile";
+import CreatePinForm from "./components/Forms/CreatePinForm";
+import PinPage from "./components/Pages/PinPage";
+import BoardDetailPage from "./components/Pages/BoardDetailPage";
+import CreateBoardForm from "./components/Forms/CreateBoardForm";
+import EditProfileForm from "./components/Forms/EditProfileForm";
+import { useParams } from "react-router-dom";
+import User from "./components/User/User";
 
 function App() {
-	const [isLoaded, setisLoaded] = useState(false);
-	const dispatch = useDispatch();
-	const sessionUser = useSelector((state) => state.session.user);
+  const [isLoaded, setisLoaded] = useState(false);
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
 
-	useEffect(() => {
-		(async () => {
-			await dispatch(authenticate());
-			setisLoaded(true);
-		})();
-	}, [dispatch]);
+  useEffect(() => {
+    (async () => {
+      await dispatch(authenticate());
+      setisLoaded(true);
+    })();
+  }, [dispatch]);
 
-	if (!isLoaded) {
-		return null;
-	}
+  if (!isLoaded) {
+    return null;
+  }
 
-	return (
-		<div>
-			<Route exact path='/'>
-				<HomePage />
-			</Route>
+  return (
+    <div>
+      <Route exact path="/">
+        <HomePage />
+      </Route>
 
-			{sessionUser && <Navigation isLoaded={isLoaded} />}
-			{isLoaded && (
-				<Switch>
-					{sessionUser && (
-						<Route exact path={`/${sessionUser.username}`}>
-							<Profile />
-						</Route>
-					)}
-					<Route exact path='/'>
-						<PinterestLayout />
-					</Route>
-					<Route exact path='/login'>
-						<LoginForm />
-					</Route>
-					<Route exact path='/signup'>
-						<SignUpForm />
-					</Route>
-					{/* <Route path='/pins'>
+      {sessionUser && <Navigation isLoaded={isLoaded} />}
+      {isLoaded && (
+        <Switch>
+          {sessionUser && (
+            <Route exact path={`/${sessionUser.username}`}>
+              <Profile />
+            </Route>
+          )}
+          <Route exact path="/">
+            <PinterestLayout />
+          </Route>
+          <Route exact path="/login">
+            <LoginForm />
+          </Route>
+          <Route exact path="/signup">
+            <SignUpForm />
+          </Route>
+          {/* <Route path='/pins'>
               <PinDetailPage />
             </Route> */}
-					={' '}
-					<ProtectedRoute exact path='/boards'>
-						<BoardDetailPage />
-					</ProtectedRoute>
-					<ProtectedRoute exact path='/boards/:boardId'>
-						<BoardDetailPage />
-					</ProtectedRoute>
-					<Route exact path='/pins'>
-						<PinPage />
-					</Route>
-					<Route exact path='/pinform'>
-						<CreatePinForm />
-					</Route>
-					<Route exact path='/boardform'>
-						<CreateBoardForm />
-					</Route>
-					<Route exact path='/profileform'>
-						<EditProfileForm />
-					</Route>
-					{/*
+          ={" "}
+          <ProtectedRoute exact path="/boards">
+            <BoardDetailPage />
+          </ProtectedRoute>
+          <ProtectedRoute exact path="/boards/:boardId">
+            <BoardDetailPage />
+          </ProtectedRoute>
+          <Route exact path="/pins">
+            <PinPage />
+          </Route>
+          <Route exact path="/pinform">
+            <CreatePinForm />
+          </Route>
+          <Route exact path="/boardform">
+            <CreateBoardForm />
+          </Route>
+          <Route exact path="/profileform">
+            <EditProfileForm />
+          </Route>
+          {/*
             <Route path='/following`'>
               <FollowingPage/>
             </Route>
@@ -87,16 +87,16 @@ function App() {
               <FollowersPage/>
             </Route>
           */}
-					<ProtectedRoute exact path='/users'>
-						<UsersList />
-					</ProtectedRoute>
-					<ProtectedRoute exact path='/users/:userId'>
-						<User />
-					</ProtectedRoute>
-				</Switch>
-			)}
-		</div>
-	);
+          <ProtectedRoute exact path="/users">
+            <UsersList />
+          </ProtectedRoute>
+          <ProtectedRoute exact path="/users/:userId">
+            <User />
+          </ProtectedRoute>
+        </Switch>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- react-app/src/store/board.js
+++ react-app/src/store/board.js
@@ -1,95 +1,97 @@
-const defaultState = {};
+const defaultState = {
+  boards: [],
+};
 
-const LOAD_BOARDS = 'boards/LOAD_BOARDS';
-const DELETE_BOARD = 'boards/DELETE_BOARD';
+const LOAD_BOARDS = "boards/LOAD_BOARDS";
+const DELETE_BOARD = "boards/DELETE_BOARD";
 
 const loadBoards = (payload) => {
-    return {
-        type: LOAD_BOARDS,
-        payload,
-    };
+  return {
+    type: LOAD_BOARDS,
+    payload,
+  };
 };
 
 const deleteBoard = (payload) => {
-    return {
-        type: DELETE_BOARD,
-        payload,
-    };
+  return {
+    type: DELETE_BOARD,
+    payload,
+  };
 };
 
 export const getAllBoardsThunk = () => async (dispatch) => {
-    const res = await fetch('/api/boards');
+  const res = await fetch("/api/boards/");
 
-    if (res.ok) {
-        const data = await res.json();
-        dispatch(loadBoards(data));
-    }
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(loadBoards(data));
+  }
 };
 
 export const createBoardThunk = (data) => async (dispatch) => {
-    const newBoard = JSON.stringify(data);
+  const newBoard = JSON.stringify(data);
 
-    const res = await fetch('/api/boards', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: newBoard,
-    });
+  const res = await fetch("/api/boards/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: newBoard,
+  });
 
-    if (res.ok) {
-        const newData = await res.json();
-        dispatch(loadBoards(newData));
-    }
+  if (res.ok) {
+    const newData = await res.json();
+    dispatch(loadBoards(newData));
+  }
 };
 
 export const editBoardThunk = (data) => async (dispatch) => {
-    const editBoard = JSON.stringify(data);
+  const editBoard = JSON.stringify(data);
 
-    const res = await fetch('/api/boards', {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: editBoard,
-    });
+  const res = await fetch("/api/boards/", {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: editBoard,
+  });
 
-    if (res.ok) {
-        const newData = await res.json();
-        dispatch(loadBoards(newData));
-    }
+  if (res.ok) {
+    const newData = await res.json();
+    dispatch(loadBoards(newData));
+  }
 };
 
 export const deleteBoardThunk = (data) => async (dispatch) => {
-    const body = JSON.stringify(data);
+  const body = JSON.stringify(data);
 
-    const res = await fetch(`/api/boards/${data.id}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body,
-    });
+  const res = await fetch(`/api/boards/${data.id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body,
+  });
 
-    if (res.ok) {
-        dispatch(deleteBoard(data.id));
-    }
+  if (res.ok) {
+    dispatch(deleteBoard(data.id));
+  }
 };
 
 const boardReducer = (state = defaultState, action) => {
-    const newState = { ...state };
+  const newState = { ...state };
 
-    switch (action.type) {
-        case LOAD_BOARDS:
-            return { ...newState, ...action.payload };
+  switch (action.type) {
+    case LOAD_BOARDS:
+      return { ...newState, ...action.payload };
 
-        case DELETE_BOARD:
-            delete newState[action.payload];
-            return newState;
+    case DELETE_BOARD:
+      delete newState[action.payload];
+      return newState;
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
 
 export default boardReducer;
