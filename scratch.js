c7c7bfc0e9d146bbc89161fc75018ff03fd57f45
ChrisC <chrischan8551@gmail.com>
2023-02-11 11:21:31 -0600

    scratch

--- scratch.js
+++ scratch.js
@@ -1,9 +1,11 @@
 //1) deploy render
+//2) replace image links with links that aren't blocked
 //2) add white space to Aman's grid
 //3) boards add real pins cards instead of fake grid
 //4) fix all the create / edit forms formatting
 //5) being able to pin (pins) to your board (***GOOD***)
 //6) edit boards format
 //7) fix the logout button where it jumps back to home page
-//8) test all pages for functionaliy or bugs
-//8) add splash page image
+//8) pins that aren't yours need to have a real link
+//9) test all pages for functionaliy or bugs
+//10) add splash page image
