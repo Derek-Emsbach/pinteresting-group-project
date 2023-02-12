ea4ee700298431da3ae3fbf9bfbf2e8663bc1adb
Aman Kaur <amanpreet_k32@yahoo.com>
2023-02-12 02:38:59 -0800

    Improve User page and enable following

--- react-app/src/components/User/User.js
+++ react-app/src/components/User/User.js
@@ -1,56 +1,111 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, useParams } from 'react-router-dom';
-import { follow, unFollowThunk } from '../../store/following_follower';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect, useHistory, useParams } from "react-router-dom";
+import { getAllPinsByAUser } from "../../store/pin";
+import {
+  getOneUserThunk,
+  followUserThunk,
+  unfollowUserThunk,
+} from "../../store/user";
+import GridLayout from "../GridLayout";
+import { AddPinningControls } from "../Pinterest_layout/PinterestLayout";
 
 function User() {
-  const [user, setUser] = useState({});
-  const { userId }  = useParams();
-  const dispatch = useDispatch()
-  const history = useHistory()
-  const sessionUser = useSelector(state=>state.session.user)
+  const { userId } = useParams();
+  const [loadingStatus, setLoadingStatus] = useState("uninitialized");
+  const [loadingError, setLoadingError] = useState(null);
+
+  const { currentUser, otherUser, isFollowing, pins } = useSelector((state) => {
+    const currentUser = state.session.user;
+    const otherUser = state.otherUser[userId];
+    const pins = Object.values(state.pin).filter(
+      ({ userId: authorId }) => `${authorId}` === `${userId}`
+    );
+
+    let isFollowing = false;
+
+    if (currentUser && otherUser) {
+      isFollowing = currentUser.following.some(
+        ({ id }) => `${id}` === `${otherUser.id}`
+      );
+    }
+
+    return {
+      otherUser,
+      currentUser,
+      isFollowing,
+      pins,
+    };
+  });
+
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   useEffect(() => {
-    if (!userId) {
-      return;
+    if (userId && !otherUser && loadingStatus === "uninitialized") {
+      dispatch(getOneUserThunk(userId));
     }
-    (async () => {
-      const response = await fetch(`/api/users/${userId}`);
-      const user = await response.json();
-      setUser(user);
-    })();
-  }, [userId]);
+  }, [userId, otherUser]);
 
-  if (!user) {
+  useEffect(async () => {
+    if (userId && loadingStatus === "uninitialized") {
+      try {
+        setLoadingStatus("pending");
+        await dispatch(getAllPinsByAUser(userId));
+        setLoadingStatus("success");
+      } catch (err) {
+        setLoadingStatus("failed");
+        setLoadingError(err);
+      }
+    }
+  }, [userId, loadingStatus]);
+
+  if (!userId) {
+    return <Redirect to="/404" />;
+  }
+
+  if (!otherUser) {
     return null;
   }
 
-  const following = async ()=>{
+  const follow = () => {
+    dispatch(followUserThunk(otherUser.id));
+  };
 
-    await dispatch(follow(user.username))
-  history.push(`/${sessionUser.username}`)
-  }
+  const unfollow = () => {
+    dispatch(unfollowUserThunk(otherUser.id));
+  };
 
-  const unFollowing = async () =>{
-    await dispatch(unFollowThunk(user.username))
-    history.push(`/${sessionUser.username}`)
-  }
+  const navigateToPinPage = (pin) => {
+    history.push(`/pins/${pin.id}`);
+  };
 
   return (
     <div>
-    <ul>
-      <li>
-        <strong>User Id</strong> {userId}
-      </li>
-      <li>
-        <strong>Username</strong> {user.username}
-      </li>
-      <li>
-        <strong>Email</strong> {user.email}
-      </li>
-    </ul>
-      <button className='create-button'onClick={following}>Follow</button>
-      <button className='regular-button' onClick={unFollowing}>UnFollow</button>
+      <ul>
+        <li>
+          <strong>User Id</strong> {userId}
+        </li>
+        <li>
+          <strong>Username</strong> {otherUser.username}
+        </li>
+        <li>
+          <strong>Email</strong> {otherUser.email}
+        </li>
+      </ul>
+      <button
+        className={isFollowing ? "regular-button" : "create-button"}
+        onClick={isFollowing ? unfollow : follow}
+      >
+        {isFollowing ? "Unfollow" : "Follow"}
+      </button>
+      <GridLayout
+        items={pins}
+        onItemClick={navigateToPinPage}
+        renderItemActions={(pin, closeActionPopOver) => (
+          <AddPinningControls pin={pin} onPinningDone={closeActionPopOver} />
+        )}
+      />
     </div>
   );
 }
