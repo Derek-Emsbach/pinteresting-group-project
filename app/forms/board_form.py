6c3664312635d668e400473eef5bdc65cdb9e187
Cherry Huang <chuang031@gmail.com>
2023-02-12 11:43:26 -0800

    error vals

--- app/forms/board_form.py
+++ app/forms/board_form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, URL
 
 class BoardForm(FlaskForm):
     title = StringField('Title',validators=[DataRequired()])
-    imageUrl = StringField('Image Url',validators=[DataRequired()])
+    imageUrl = StringField('Image Url',validators=[DataRequired(), URL( message='This is not a valid image link, make sure you enter the entire image URL')])
