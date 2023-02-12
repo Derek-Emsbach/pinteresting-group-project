2c22c60aadb51b1b99ce66eba52b16fbf47f98e6
Derek Emsbach <demsbach92@gmail.com>
2023-02-12 16:11:16 -0700

    added missing create board button to profile

--- react-app/src/components/Pages/ProfilePage/Profile.js
+++ react-app/src/components/Pages/ProfilePage/Profile.js
@@ -5,111 +5,135 @@ import { useHistory } from "react-router-dom";
 import Followers from "./Followers";
 import Followings from "./Followings";
 import { selectMyBoards } from "../../../store/board";
-import './Profile.css'
+import "./Profile.css";
 
 function Profile() {
-  const currentUser = useSelector((state) => state.session.user);
-  const [openMyBoards,setOpenMyBoards] = useState(true);
-  const [openCreate, setOpenCreate] = useState(false);
-  const history = useHistory();
-  const allMyBoards = useSelector(selectMyBoards)
-  console.log(allMyBoards)
+	const currentUser = useSelector((state) => state.session.user);
+	const [openMyBoards, setOpenMyBoards] = useState(true);
+	const [openCreate, setOpenCreate] = useState(false);
+	const history = useHistory();
+	const allMyBoards = useSelector(selectMyBoards);
+	console.log(allMyBoards);
 
-  const myPins = async (e) => {
-    history.push("/pins");
-  };
-  const myBoards = async (e) => {
-    history.push("/boards");
-  };
+	const myPins = async (e) => {
+		history.push("/pins");
+	};
+	const myBoards = async (e) => {
+		history.push("/boards");
+	};
 
-  const profileForm = async (e) => {
-    history.push(`/users/${currentUser.id}`);
-  };
+	const profileForm = async (e) => {
+		history.push(`/users/${currentUser.id}`);
+	};
 
-  if (!currentUser) {
-    return null;
-  }
+	if (!currentUser) {
+		return null;
+	}
 
-  if(!allMyBoards){
-    return null
-  }
+	if (!allMyBoards) {
+		return null;
+	}
 
-  return (
-    <div className="profile_container">
-      <div className="user_info">
-        <img src={currentUser.image}></img>
-        <div className="profile_info">
-          <h1>
-            {currentUser.firstName} {currentUser.lastName}
-          </h1>
-          <h3>{currentUser.about}</h3>
-          <h4>{currentUser.email}</h4>
-        </div>
+	return (
+		<div className="profile_container">
+			<div className="user_info">
+				<img src={currentUser.image}></img>
+				<div className="profile_info">
+					<h1>
+						{currentUser.firstName} {currentUser.lastName}
+					</h1>
+					<h3>{currentUser.about}</h3>
+					<h4>{currentUser.email}</h4>
+				</div>
 
-        <div className="follow-container">
-          <div className="follower_container">
-            {currentUser.followers.length}
+				<div className="follow-container">
+					<div className="follower_container">
+						{currentUser.followers.length}
 
-            <Followers />
-          </div>
+						<Followers />
+					</div>
 
-          <div className="following_container">
-            {currentUser.following.length}
+					<div className="following_container">
+						{currentUser.following.length}
 
-            <Followings />
-          </div>
-        </div>
+						<Followings />
+					</div>
+				</div>
 
-        <button className="regular-button" onClick={myPins}>
-          My Pins
-        </button>
-        <button className="regular-button" onClick={profileForm}>
-          Edit Profile
-        </button>
+				<button className="regular-button" onClick={myPins}>
+					My Pins
+				</button>
+				<button className="regular-button" onClick={profileForm}>
+					Edit Profile
+				</button>
+			</div>
 
-      </div>
+			<div className="option">
+				<div className="option_btn">
+					<button
+						onClick={() => {
+							setOpenCreate(true);
+							setOpenMyBoards(false);
+						}}
+						className={`button ${openCreate ? "active" : null}`}
+					>
+						{" "}
+						<span>Create</span>
+					</button>
+					<button
+						onClick={() => {
+							setOpenMyBoards(true);
+							setOpenCreate(false);
+						}}
+						className={`button ${openMyBoards ? "active" : null}`}
+					>
+						<span>My boards</span>{" "}
+					</button>
+				</div>
+			</div>
+			<div className="all_my_boardImg">
+				{openMyBoards &&
+					allMyBoards &&
+					allMyBoards.map((board) => {
+						return (
+							<button
+								className="all_board_container"
+								onClick={() =>
+									history.push(`/boards/${board.id}`)
+								}
+							>
+								<div>
+									<img src={board.imageUrl} alt="" />
+								</div>
+								<div>{board.title}</div>
+							</button>
+						);
+					})}
+			</div>
 
-      
-      <div className="option">
-        
-      <div className="option_btn">
-         
-      <button onClick={()=>{setOpenCreate(true); setOpenMyBoards(false)}} className={ `button ${openCreate ? 'active' : null}`}> <span>Create</span></button>
-      <button onClick={()=>{setOpenMyBoards(true); setOpenCreate(false)}} className={ `button ${openMyBoards ? 'active' : null}`}><span>My boards</span> </button>
-      </div>
-
-      </div>
-      <div className="all_my_boardImg">
-      {openMyBoards && allMyBoards &&(
-        allMyBoards.map(board =>{
-                  return(
-                  <button className="all_board_container" onClick={()=>history.push(`/boards/${board.id}`)}>
-                      <div>
-                      <img src={board.imageUrl} alt=''/>
-                      </div>
-                      <div>
-                          {board.title}
-                      </div>                                     
-                  </button>
-                  )
-              })
-             
-          )
-
-      }
-      </div>
-
-      {openCreate &&(
-             <div className="create_pin_container">
-             <h1> Inspire with an Idea Pin</h1>
-             <button onClick={()=> history.push('/pinform')} className='create_pin_btn'>Create</button>
-             </div>
-         
-      )}
-
-
-
-    </div>
-  );
+			{openCreate && (
+				<>
+					<div className="create_pin_container">
+						<h2> Inspire with an Idea Pin</h2>
+						<button
+							onClick={() => history.push("/pinform")}
+							className="create_pin_btn"
+						>
+							Create
+						</button>
+					</div>
+					<div className="create_board_container">
+						<h2> Create a Board </h2>
+						<button
+							onClick={() => history.push("/boardform")}
+							className="create_pin_btn"
+						>
+							Create
+						</button>
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
 export default Profile;
